b6d852003160ab29cc3275240ce83231cfa26746
manu-bitt <manupal8954@gmail.com>
2025-10-28 14:58:43 +0530

    Fix formatting in Blog.js schema definition

--- backend/models/Blog.js
+++ backend/models/Blog.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const blogSchema = new mongoose.Schema({
-  userId: { type: String, required: true }, // store username or later, userId from auth
+  userId: { type: String, required: true }, 
   title: { type: String, required: true },
   content: { type: String, required: true },
   tags: [String],
